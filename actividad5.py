f2ff72cff1299d16ca982c7086df7df02d55c401
Williams-14 <129902651+Williams-14@users.noreply.github.com>
2023-05-09 23:06:56 -0600

    Update actividad5.py

--- actividad5.py
+++ actividad5.py
@@ -1,8 +1,12 @@
+Escribe una función que pueda decirte si un año (número entero) es bisiesto o no.
+
+
+
 def bisiesto(año):
     if año % 4 ==0:
         print(f"{año} es bisiesto")
     else:
         print(f"{año} no es bisiesto")
   
-  
-bisiesto(2024)
+num = int(input("Ingrese un año por favor:\n"))
+bisiesto(num)
